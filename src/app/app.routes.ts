5a956e3d44e574d1233bb40ec2237df2f48008b2
barni246 <barnabas.gonda@googlemail.com>
2026-01-03 10:31:49 +0100

    seo component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { Products } from './products/products';
 import { Imprint } from './imprint/imprint';
 import { Dataprotection } from './dataprotection/dataprotection';
+import { Seo } from './seo/seo';
 
 
 
 export const routes: Routes = [
 
   { path: '', component: Products },
+   { path: 'seo', component: Seo },
   { path: 'imprint', component: Imprint },
   { path: 'dataprotection', component: Dataprotection },
   { path: '**', redirectTo: '', pathMatch: 'full' }
